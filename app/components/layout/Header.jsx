3420730fd88ea130660e18f4e06fa2d336befae4
KnightlyDev <kolt67@ukr.net>
2026-02-03 02:01:30 +0200

    feat: Added Sigh in/up buttons in header. Added logout button in header if user auth

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -1,51 +1,44 @@
 // app/components/layout/Header.jsx
-import { useState } from "react";
-import { Link } from "react-router";
+import { useCallback, useEffect, useState } from "react";
+import { Link, NavLink, useRouteLoaderData, useSubmit } from "react-router";
 import { useMediaQuery } from "react-responsive";
 import { tailwindBreakpoints } from "@constants/tailwindBreakpoints";
 import NavigateLinks from "@components/ui/Links/NavLinks";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { cn } from "@utils/cn";
+import HoldButton from "@components/ui/Buttons/HoldButton";
 
-export default function Header() {
+const NavMenu = ({ children }) => {
   const [isBurgerMenuOpen, setBurgerMenu] = useState(false);
 
   const isMobile = useMediaQuery({ maxWidth: tailwindBreakpoints.md - 1 });
+  useEffect(() => {
+    if (!isMobile) {
+      return () => {
+        document.body.style.overflow = "";
+      };
+    }
+    if (isBurgerMenuOpen) {
+      document.body.style.overflow = "hidden";
+      return () => {
+        document.body.style.overflow = "";
+      };
+    }
+  }, [isBurgerMenuOpen, isMobile]);
 
-  const toggleBurgerMenu = () => {
-    setBurgerMenu((prev) => {
-      const newState = !prev;
-      // Maybe will be moved higher in data flow because blocks body.
-      document.body.style.overflow = newState ? "hidden" : "";
-      return newState;
-    });
-  };
-
-  const closeBurgerMenu = () => {
-    setBurgerMenu(false);
-    document.body.style.overflow = "";
-  };
+  const toggleBurgerMenu = () => setBurgerMenu((prev) => !prev);
+  const closeBurgerMenu = () => setBurgerMenu(false);
 
   return (
-    <header className="bg-surface border-b-secondary flex items-center justify-between gap-4 rounded-b-xl border-b-2 p-4 transition-all duration-300">
-      <Link to="/" onClick={closeBurgerMenu}>
-        <h1 className="text-text-primary text-2xl font-bold">LuminaBookie</h1>
-      </Link>
-
+    <>
       {isMobile ? (
-        <div className="md:hidden">
+        <div>
           <button
             onClick={toggleBurgerMenu}
             className="text-text-primary hover:text-primary p-2 transition-colors"
             aria-label="Toggle menu"
           >
-            {/* Burger icon */}
-            <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0  0 24 24">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            <Bars3Icon className="size-6" />
           </button>
 
           {/* Mobile menu overlay */}
@@ -56,26 +49,70 @@ export default function Header() {
                 className="text-text-primary hover:text-primary absolute top-4 right-4 p-2 transition-colors"
                 aria-label="Close menu"
               >
-                <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
+                <XMarkIcon className="size-6" />
               </button>
 
               {/* Menu content */}
-              <div className="flex h-full items-center justify-center">
-                <NavigateLinks className={"flex flex-col items-center gap-6"} />
-              </div>
+              <NavigateLinks className={"flex h-full flex-col items-center justify-center gap-6"}>
+                {children}
+              </NavigateLinks>
             </div>
           )}
         </div>
       ) : (
-        <NavigateLinks className="flex items-center gap-6" />
+        <NavigateLinks className="flex flex-row-reverse items-center gap-6">
+          {children}
+        </NavigateLinks>
       )}
+    </>
+  );
+};
+
+export default function Header() {
+  const { user } = useRouteLoaderData("root");
+
+  const submit = useSubmit();
+  const logOutFunction = () => {
+    submit(null, { action: "/api/logout", method: "post" });
+  };
+
+  return (
+    <header className="bg-surface border-b-secondary flex items-center justify-between gap-4 rounded-b-xl border-b-2 p-4 transition-all duration-300">
+      <Link to="/">
+        <h1 className="text-text-primary text-2xl font-bold">LuminaBookie</h1>
+      </Link>
+      <NavMenu>
+        {/*Login/Register section */}
+        <div
+          className={cn(
+            "flex flex-col items-center gap-2 md:flex-row",
+            "*:border-primary *:rounded-md *:border-2 *:px-2 *:py-2 *:transition-all *:hover:scale-105 *:hover:shadow-2xs md:*:px-4"
+          )}
+        >
+          <NavLink
+            to="login"
+            className={({ isActive }) =>
+              cn((isActive || user) && "hidden", "bg-surface text-primary hover:bg-primary/10")
+            }
+          >
+            Sign in
+          </NavLink>
+
+          <NavLink
+            to="register"
+            className={({ isActive }) =>
+              cn((isActive || user) && "hidden", "bg-primary hover:bg-primary-hover text-white")
+            }
+          >
+            Sign up
+          </NavLink>
+          {user && (
+            <HoldButton duration={600} onComplete={logOutFunction}>
+              Log out
+            </HoldButton>
+          )}
+        </div>
+      </NavMenu>
     </header>
   );
 }
